2e775b43f9a6bbd147aa318a68636d57111ddc08
Your Name <you@example.com>
2025-02-27 11:49:11 +1000

    fix: properly format rollup.config.js file

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,30 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve'; import commonjs from '@rollup/plugin-commonjs'; import { babel } from '@rollup/plugin-babel'; import terser from '@rollup/plugin-terser'; import postcss from 'rollup-plugin-postcss'; export default { input: 'app/page.tsx', output: { dir: 'dist', format: 'esm', sourcemap: true }, plugins: [ nodeResolve({ extensions: ['.js', '.jsx', '.ts', '.tsx'] }), commonjs(), babel({ babelHelpers: 'bundled', exclude: 'node_modules/**', extensions: ['.js', '.jsx', '.ts', '.tsx'] }), postcss({ extensions: ['.css'], minimize: true }), terser() ] };
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+  input: 'app/page.tsx',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    sourcemap: true
+  },
+  plugins: [
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }),
+    postcss({
+      extensions: ['.css'],
+      minimize: true
+    }),
+    terser()
+  ]
+}; 
