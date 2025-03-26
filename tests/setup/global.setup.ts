2b15e40af57dfcf8cc164391610a60f82bf3c233
HDTS69 <admin@hdtradeservices.com.au>
2025-03-26 17:55:27 +1000

    refactor: remove Playwright tests and configurations

--- tests/setup/global.setup.ts
+++
@@ -1,6 +0,0 @@
-import { test as setup, expect } from '@playwright/test';
-
-setup('do global setup', async ({ page }) => {
-  // Add any global setup here
-  console.log('Running global setup...');
-}); 
